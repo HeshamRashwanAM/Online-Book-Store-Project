701426f4415e73f91066548b65a66ce7db05f710
b3nj5m1n <47924309+b3nj5m1n@users.noreply.github.com>
2019-03-15 22:25:38 +0100

    Init Commit

--- Online Book Store (OOPs)/Online Book Store (OOPs)/The Main/Program.cs
+++ Online Book Store (OOPs)/Online Book Store (OOPs)/The Main/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Online_Book_Store__OOPs_.Classes;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,10 +31,64 @@ namespace Online_Book_Store__OOPs_
     */
     class Program
     {
+        static List<User_Object> users;
         static void Main(string[] args)
         {
-            User admin = new Reader("Hesham","01142428227","heshamrashwanali@gmail.com");
-            admin.DisplayBooks();
+            //User admin = new Reader("Hesham","01142428227","heshamrashwanali@gmail.com");
+            //admin.DisplayBooks();
+
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "users.txt"))
+            {
+                File.Create(AppDomain.CurrentDomain.BaseDirectory + "users.txt");
+            }
+            users = new List<User_Object>();
+            users = Parser.ReadUser_Objects();
+
+            Console.WriteLine("Type L to list all users");
+            Console.WriteLine("Type N to add a new User");
+
+            string cmd = Console.ReadLine();
+            if (cmd == "L")
+            {
+                ListUsers();
+                Main(null);
+            }
+            if (cmd == "N")
+            {
+                CreateNewUser();
+                Main(null);
+            }
+        }
+
+        static void ListUsers()
+        {
+            users = new List<User_Object>();
+            users = Parser.ReadUser_Objects();
+            foreach (var item in users)
+            {
+                Console.WriteLine("\nUser:\n Username: " + item.Username + "\n Password: " + item.Password);
+            }
+        }
+        static void CreateNewUser()
+        {
+            users = new List<User_Object>();
+            users = Parser.ReadUser_Objects();
+            Console.WriteLine("New username: ");
+            string n_username = Console.ReadLine();
+
+            Console.WriteLine("New password: ");
+            string n_password = Console.ReadLine();
+            User_Object u = new User_Object();
+            u.Username = n_username;
+            u.Password = n_password;
+            users.Add(u);
+            update();
+            Parser.WriteUser_Objects();
+
+        }
+        static void update()
+        {
+            Parser.User_Objects = users;
         }
     }
 }
